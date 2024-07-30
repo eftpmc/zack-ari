5c50fb5db20900fe6be0ec35616321e937e85598
SaturnEFTPMC <zackhhi@gmail.com>
2024-07-30 00:16:25 -0400

    intro responsiveness

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -11,8 +11,8 @@ const Intro = () => {
   return (
     <section className="bg-base-200 text-base-content p-8 flex items-center justify-center min-h-[calc(100vh-60px)]">
       <div className="flex flex-col items-center text-center">
-        <h1 className="intro-title text-4xl font-bold font-squarechunks mb-2">Hi, I'm Zack</h1>
-        <h2 className="intro-subtitle text-5xl font-starblues text-primary mb-6 whitespace-nowrap">Fullstack Developer</h2>
+        <h1 className="intro-title text-2xl md:text-4xl font-bold font-squarechunks mb-2">Hi, I'm Zack</h1>
+        <h2 className="intro-subtitle text-3xl md:text-5xl font-starblues text-primary mb-6 whitespace-nowrap">Fullstack Developer</h2>
       </div>
     </section>
   );
